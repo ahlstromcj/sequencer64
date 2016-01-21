c73db18335e8f1a5a85f59f88f158a29c2ffd120
Chris Ahlstrom <ahlstrom@centiped>
2016-01-21 12:47:24 -0500

    Found incorrect parameter order for delta time function in perform, possibly causing bad tick increment and pill bug.

--- libseq64/src/perform.cpp
+++ libseq64/src/perform.cpp
@@ -1820,9 +1820,13 @@ perform::output_func ()
              * Delta time to ticks; get delta ticks.
              *
              * double delta_tick = double(bpm * ppqn * (delta_us / 60000000.0f));
+             *
+             * @change ca 2016-01-21  Doh!  Wrong parameter order:
+             *
+             * double delta_tick = delta_time_us_to_ticks(bpm, ppqn, delta_us);
              */
 
-            double delta_tick = delta_time_us_to_ticks(bpm, ppqn, delta_us);
+            double delta_tick = delta_time_us_to_ticks(delta_us, bpm, ppqn);
             if (m_usemidiclock)
             {
                 delta_tick = m_midiclocktick;
--- libseq64/src/perfstats.cpp
+++ libseq64/src/perfstats.cpp
@@ -407,7 +407,7 @@ perfstats::output_func ()
 #endif
 
 //          int bpm  = m_master_bus.get_beats_per_minute();
-//          double delta_tick = delta_time_us_to_ticks(bpm, ppqn, delta_us);
+//          double delta_tick = delta_time_us_to_ticks(delta_us, bpm, ppqn);
 //
 //              play(long(pad.js_current_tick));                // play!
 //              m_master_bus.clock(long(pad.js_clock_tick));    // MIDI clock

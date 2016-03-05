6e91d527ea6fee45b9baeb1291a3a81dfcefd254
Chris Ahlstrom <ahlstromcj@gmail.com>
2016-03-04 20:10:28 -0500

    Prepping for version 0.9.10.1 in fixups branch.

--- libseq64/src/cmdlineopts.cpp
+++ libseq64/src/cmdlineopts.cpp
@@ -25,7 +25,7 @@
  * \library       sequencer64 application
  * \author        Seq24 team; modifications by Chris Ahlstrom
  * \date          2015-11-20
- * \updates       2016-02-21
+ * \updates       2016-03-04
  * \license       GNU GPLv2 or above
  *
  *  The "rc" command-line options override setting that are first read from
@@ -141,7 +141,7 @@ static const std::string s_arg_list =
     ;
 
 static const char * const s_help_1a =
-"sequencer64 v 0.9.10.0 A significant refactoring of the seq24 live sequencer.\n"
+"sequencer64 v 0.9.10.1 A significant refactoring of the seq24 live sequencer.\n"
 "\n"
 "Usage: sequencer64 [options] [MIDI filename]\n\n"
 "Options:\n"

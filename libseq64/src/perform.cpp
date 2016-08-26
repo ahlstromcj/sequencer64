ahlstromcj/sequencer64

commit a9e14fe5cfd8f03b65d2acb1b2710ed9e5637463

Enabled experimental screen-set code for testing, see USE_EXPERIMENTAL_CODE macro usage.

libseq64/src/perform.cpp
@@ -45,6 +45,8 @@
 #include "perform.hpp"
 #include "settings.hpp"                 /* seq64::rc() and choose_ppqn()    */
 
+#define USE_EXPERIMENTAL_CODE
+
 /**
  *  Provide a "not-a-number" macro.  -nan(0x8000000000000)
  */
@@ -2533,6 +2535,7 @@ perform::input_func ()
                         {
                             for (int i = 0; i < c_midi_controls; ++i)
                             {
+                                int offset = m_offset + 1;
                                 midibyte data[2] = { 0, 0 };
                                 midibyte status = ev.get_status();
                                 ev.get_data(data[0], data[1]);
@@ -2541,7 +2544,7 @@ perform::input_func ()
                                     if (midi_control_toggle(i).in_range(data[1]))
                                     {
                                         if (i < m_seqs_in_set)
-                                            sequence_playing_toggle(i+m_offset);
+                                            sequence_playing_toggle(offset);
                                     }
                                 }
                                 if (midi_control_on(i).match(status, data[0]))
@@ -2549,14 +2552,14 @@ perform::input_func ()
                                     if (midi_control_on(i).in_range(data[1]))
                                     {
                                         if (i < m_seqs_in_set)
-                                            sequence_playing_on(i+m_offset);
+                                            sequence_playing_on(offset);
                                         else
                                             handle_midi_control(i, true);
                                     }
                                     else if (midi_control_on(i).inverse_active())
                                     {
                                         if (i < m_seqs_in_set)
-                                            sequence_playing_off(i+m_offset);
+                                            sequence_playing_off(offset);
                                         else
                                             handle_midi_control(i, false);
                                     }
@@ -2566,14 +2569,14 @@ perform::input_func ()
                                     if (midi_control_on(i).in_range(data[1]))
                                     {
                                         if (i < m_seqs_in_set)
-                                            sequence_playing_off(i+m_offset);
+                                            sequence_playing_off(offset);
                                         else
                                             handle_midi_control(i, false);
                                     }
                                     else if (midi_control_off(i).inverse_active())
                                     {
                                         if (i < m_seqs_in_set)
-                                            sequence_playing_on(i+m_offset);
+                                            sequence_playing_on(offset);
                                         else
                                             handle_midi_control(i, true);
                                     }

libseq64/include/perform.hpp
@@ -503,7 +503,10 @@ private:
     midi_control m_midi_cc_off[c_midi_controls];
 
     /**
-     *  Holds the offset into the screen sets.
+     *  Holds the current offset into the screen-sets.  It is used in the MIDI
+     *  control of the playback status of the sequences in the current
+     *  screen-set.  It is also used to offset the sequence numbers so that
+     *  the control (mute/unmute) keys can be shown on any screen-set.
      */
 
     int m_offset;

seq_gtkmm2/include/mainwid.hpp
@@ -28,7 +28,7 @@
  * \library       sequencer64 application
  * \author        Seq24 team; modifications by Chris Ahlstrom
  * \date          2015-07-24
- * \updates       2016-07-05
+ * \updates       2016-07-12
  * \license       GNU GPLv2 or above
  *
  */
@@ -36,6 +36,8 @@
 #include "globals.h"                    /* c_max_sequence, etc.     */
 #include "gui_drawingarea_gtk2.hpp"     /* one base class           */
 #include "seqmenu.hpp"                  /* the other base class     */
+
+#define USE_EXPERIMENTAL_CODE
 
 namespace seq64
 {
@@ -151,7 +153,11 @@ public:
     mainwid (perform & p);
     virtual ~mainwid ();
 
+#ifdef USE_EXPERIMENTAL_CODE
+    void set_screenset (int ss, bool setperf = false);
+#else
     void set_screenset (int ss);                /* int get_screenset() ?    */
+#endif
 
 private:
 

seq_gtkmm2/src/mainwid.cpp
@@ -51,6 +51,8 @@
 #include "perform.hpp"                  /* seq64::perform music control     */
 #include "settings.hpp"                 /* seq64::usr()                     */
 
+#define USE_EXPERIMENTAL_CODE
+
 namespace seq64
 {
 

seq_gtkmm2/src/mainwnd.cpp
@@ -106,6 +106,8 @@
 
 using namespace Gtk::Menu_Helpers;      /* MenuElem, etc.                */
 
+#define USE_EXPERIMENTAL_CODE
+
 namespace seq64
 {
 
@@ -544,7 +546,7 @@ mainwnd::timer_callback ()
     if (m_adjust_ss->get_value() != screenset)
     {
         m_main_wid->set_screenset(screenset);
-        
+
         /*
          * Shouldn't we call this here?  No matter, we are going to fold this
          * call into the call above.
@@ -1103,7 +1105,7 @@ void
 mainwnd::adj_callback_ss ()
 {
 #ifdef USE_EXPERIMENTAL_CODE
-    m_main_wid->set_screenset(int(m_adjust_ss->get_value()));
+    m_main_wid->set_screenset(int(m_adjust_ss->get_value()), true);
 #else
     perf().set_screenset(int(m_adjust_ss->get_value()));
     m_main_wid->set_screenset(perf().get_screenset());

seq_gtkmm2/src/seqroll.cpp
@@ -1237,7 +1237,7 @@ seqroll::move_selection_box (int dx, int dy)
     m_seqkeys_wid.set_hint_key(note);
     snap_x(m_current_x);
 
-#ifdef USE_EXPERIMENTAL_CODE
+#ifdef USE_EXPERIMENTAL_CODE_XXX
     int x0 = m_old.x;
     int y0 = m_old.y;
     int w = m_old.width;

commit 7dd521ea1c5ff2b447e740bc971ce2a3a298bb04

Fix for issue "MIDI controller toggles the wrong pattern" ahlstromcj/sequencer64#23 Regression introduced in commit a9e14fe5cfd8f03b65d2acb1b2710ed9e5637463

libseq64/src/perform.cpp
@@ -2660,7 +2660,7 @@ perform::input_func ()
                         {
                             for (int i = 0; i < c_midi_controls; ++i)
                             {
-                                int offset = m_offset + 1;
+                                int offset = m_offset + i;
                                 midibyte data[2] = { 0, 0 };
                                 midibyte status = ev.get_status();
                                 ev.get_data(data[0], data[1]);
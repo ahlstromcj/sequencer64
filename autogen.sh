cd8f1a38fd1e1752449859d4735dffc4d966d780
Chris Ahlstrom <ahlstromcj@gmail.com>
2016-11-19 19:51:10 -0500

    Tweaks to some of the build scripts.

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+#  Provides a basic autogen.sh, which uses our bootstrap script to do
+#  all the work.  Run ./bootstrap --help if you want to customize the
+#  build.
+
+./bootstrap --enable-release
+
+# vim: ts=4 sw=4 et ft=sh

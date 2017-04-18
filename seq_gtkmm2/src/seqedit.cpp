04b2f15aafdd1e815c41a96f681239ec5f11f235
Chris Ahlstrom <ahlstromcj@gmail.com>
2017-04-18 06:21:06 -0400

    Tentative fix to issue #69, MIDI thru not working.

--- seq_gtkmm2/src/seqedit.cpp
+++ seq_gtkmm2/src/seqedit.cpp
@@ -25,7 +25,7 @@
  * \library       sequencer64 application
  * \author        Seq24 team; modifications by Chris Ahlstrom
  * \date          2015-07-24
- * \updates       2017-03-25
+ * \updates       2017-04-18
  * \license       GNU GPLv2 or above
  *
  *  Compare this class to eventedit, which has to do some similar things,
@@ -2256,7 +2256,7 @@ seqedit::thru_change_callback ()
     if (! record_active)
         perf().master_bus().set_sequence_input(thru_active, &m_seq);
 
-    m_seq.set_recording(thru_active);
+    m_seq.set_thru(thru_active);        /* issue #69 fixed here */
 }
 
 /**
